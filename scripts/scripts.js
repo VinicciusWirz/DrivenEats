b27c69f51a7a246c182c76c8269a63203b9f2f11
Viniccius Wirz <vinimovel23@gmail.com>
2023-01-06 00:22:20 -0300

    Adicionado storage de variaveis e aplicação da função this

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,64 +10,91 @@ let nome = "";
 
 //Função para selecionar os items do menu de Pratos Principais
 function menuPrincipal(selecionado){
-    const elementPrincipal = document.querySelector('.opcoesPrincipais>.menu-selected');
-    const element = document.querySelector(selecionado);
-    const checkmark = document.querySelector(selecionado + ">ion-icon");
-
-    if (elementPrincipal !== null){
-        document.querySelector('.opcoesPrincipais>.menu-selected>ion-icon').classList.add('hide');
-        elementPrincipal.classList.remove('menu-selected');
+   const elementPrincipal = document.querySelector('.opcoesPrincipais>.menu-selected');
+   
+   if(precoPrincipal !== 0 ){
+    precoPrincipal = 0;
     }
-    if(elementPrincipal == element){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
-    } else if (element.classList.contains('menu-selected')){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
-    } else {
-        element.classList.add('menu-selected');
-        checkmark.classList.remove('hide');
-    }   
+   if(elementPrincipal !== null){
+    elementPrincipal.classList.remove('menu-selected');
+    elementPrincipal.querySelector('ion-icon').classList.add('hide');
+    if(numeroSelecoes.includes(1)){
+        numeroSelecoes.splice(numeroSelecoes.indexOf(1), 1);
+    }
+   } 
+   if(elementPrincipal == selecionado){
+    selecionado.classList.remove('menu-selected');
+    selecionado.querySelector('ion-icon').classList.add('hide');
+   } else if (selecionado.classList.contains('menu-selected')){
+    selecionado.classList.remove('menu-selected');
+    selecionado.querySelector('ion-icon').classList.add('hide');
+   } else {
+    selecionado.classList.add('menu-selected');
+    selecionado.querySelector('ion-icon').classList.remove('hide');
+    if(!numeroSelecoes.includes(1)){
+        numeroSelecoes.push(1);
+    }
+    precoPrincipal = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')).toFixed(2);
+    nomePrincipal = selecionado.querySelector('h4').innerHTML;
+   }
 }
 //Função para selecionar os items do menu de Bebidas
 function menuBebidas(selecionado){
     const elementPrincipal = document.querySelector('.opcoesBebidas>.menu-selected');
-    const element = document.querySelector(selecionado);
-    const checkmark = document.querySelector(selecionado + ">ion-icon");
-
-    if (elementPrincipal !== null){
-        document.querySelector('.opcoesBebidas>.menu-selected>ion-icon').classList.add('hide');
-        elementPrincipal.classList.remove('menu-selected');
-    }
-    if(elementPrincipal == element){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
-    } else if (element.classList.contains('menu-selected')){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
+   
+    if(precoBebida !== 0 ){
+     precoBebida = 0;
+     }
+    if(elementPrincipal !== null){
+     elementPrincipal.classList.remove('menu-selected');
+     elementPrincipal.querySelector('ion-icon').classList.add('hide');
+     if(numeroSelecoes.includes(2)){
+         numeroSelecoes.splice(numeroSelecoes.indexOf(2), 1);
+     }
+    } 
+    if(elementPrincipal == selecionado){
+     selecionado.classList.remove('menu-selected');
+     selecionado.querySelector('ion-icon').classList.add('hide');
+    } else if (selecionado.classList.contains('menu-selected')){
+     selecionado.classList.remove('menu-selected');
+     selecionado.querySelector('ion-icon').classList.add('hide');
     } else {
-        element.classList.add('menu-selected');
-        checkmark.classList.remove('hide');
+     selecionado.classList.add('menu-selected');
+     selecionado.querySelector('ion-icon').classList.remove('hide');
+     if(!numeroSelecoes.includes(2)){
+         numeroSelecoes.push(2);
+     }
+     precoBebida = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')).toFixed(2);
+     nomeBebida = selecionado.querySelector('h4').innerHTML;
     }
 }
 //Função para selecionar os items do menu de Sobremesas
 function menuSobremesas(selecionado){
     const elementPrincipal = document.querySelector('.opcoesSobremesas>.menu-selected');
-    const element = document.querySelector(selecionado);
-    const checkmark = document.querySelector(selecionado + ">ion-icon");
-
-    if (elementPrincipal !== null){
-        document.querySelector('.opcoesSobremesas>.menu-selected>ion-icon').classList.add('hide');
-        elementPrincipal.classList.remove('menu-selected');
-    }
-    if(elementPrincipal == element){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
-    } else if (element.classList.contains('menu-selected')){
-        element.classList.remove('menu-selected');
-        checkmark.classList.add('hide');
+   
+    if(precoSobremesa !== 0 ){
+     precoSobremesa = 0;
+     }
+    if(elementPrincipal !== null){
+     elementPrincipal.classList.remove('menu-selected');
+     elementPrincipal.querySelector('ion-icon').classList.add('hide');
+     if(numeroSelecoes.includes(3)){
+         numeroSelecoes.splice(numeroSelecoes.indexOf(3), 1);
+     }
+    } 
+    if(elementPrincipal == selecionado){
+     selecionado.classList.remove('menu-selected');
+     selecionado.querySelector('ion-icon').classList.add('hide');
+    } else if (selecionado.classList.contains('menu-selected')){
+     selecionado.classList.remove('menu-selected');
+     selecionado.querySelector('ion-icon').classList.add('hide');
     } else {
-        element.classList.add('menu-selected');
-        checkmark.classList.remove('hide');
+     selecionado.classList.add('menu-selected');
+     selecionado.querySelector('ion-icon').classList.remove('hide');
+     if(!numeroSelecoes.includes(3)){
+         numeroSelecoes.push(3);
+     }
+     precoSobremesa = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')).toFixed(2);
+     nomeSobremesa = selecionado.querySelector('h4').innerHTML;
     }
 }
