6efc30a635ac30822a435c824cd9354bd83e6b6f
Viniccius Wirz <vinimovel23@gmail.com>
2023-01-07 19:27:21 -0300

    Refatora código para melhorar performance, formata arquivos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,145 +1,129 @@
-const numeroSelecoes = [0, 0, 0];
-let nomePrincipal = "";
-let precoPrincipal = 0;
-let nomeBebida = "";
-let precoBebida = 0;
-let nomeSobremesa = "";
-let precoSobremesa = 0;
-let subTotal = 0;
-let clientName = "Não fornecido";
-let clientAddress = "Não fornecido";
-//Função para selecionar os items do menu de Pratos Principais
-function menuPrincipal(selecionado){
-   const elementPrincipal = document.querySelector('.opcoesPrincipais>.menu-selected');
-   const menuSelected = 'menu-selected';
-   if(precoPrincipal !== 0 ){
-    precoPrincipal = 0;
-    }
-   if(elementPrincipal !== null){
-    elementPrincipal.classList.remove(menuSelected);
-    elementPrincipal.querySelector('ion-icon').classList.add('hide');
-    if(numeroSelecoes.includes(1)){
-        numeroSelecoes[0] = 0;
-    }
-   }
-   if(elementPrincipal === selecionado || selecionado.classList.contains(menuSelected)){
-    selecionado.classList.remove(menuSelected);
-    selecionado.querySelector('ion-icon').classList.add('hide');
-   } else {
-    selecionado.classList.add(menuSelected);
-    selecionado.querySelector('ion-icon').classList.remove('hide');
-    if(!numeroSelecoes.includes(1)){
-        numeroSelecoes[0] = 1;
-    }
-    precoPrincipal = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')*100);
-    nomePrincipal = selecionado.querySelector('h4').innerHTML;
-   }
-   checkingOut();
-}
-//Função para selecionar os items do menu de Bebidas
-function menuBebidas(selecionado){
-    const elementPrincipal = document.querySelector('.opcoesBebidas>.menu-selected');
+//Nova Variavel [nome dish, preço dish, nome drink, preço drink, nome dessert, preço dessert, soma]
+const arrOrderData = ['', 0, '', 0, '', 0, 0];
+const noInput = 'Não fornecido';
+let clientName = noInput;
+let clientAddress = noInput;
+//Função para selecionar Items do menu
+function menuCall(touch) {
     const menuSelected = 'menu-selected';
-    if(precoBebida !== 0 ){
-     precoBebida = 0;
+    const elementPastSelected = touch.parentElement.querySelector('.' + menuSelected);
+    const touchClassCheck = elementPastSelected === touch;
+    const placeNameInArray = touch.querySelector('h4').innerHTML;
+    const placePriceInArray = Number(touch.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.'));
+    if (touch.getAttribute('data-test') === 'dish') {
+        if (arrOrderData[1] !== 0) {
+            arrOrderData[0] = '';
+            arrOrderData[1] = 0;
+        }
+        if (elementPastSelected !== null) {
+            elementPastSelected.classList.remove(menuSelected);
+            elementPastSelected.querySelector('ion-icon').classList.add('hide');
+        }
+        if (touchClassCheck) {
+            touch.classList.remove(menuSelected);
+            touch.querySelector('ion-icon').classList.add('hide');
+        } else {
+            touch.classList.add(menuSelected);
+            touch.querySelector('ion-icon').classList.remove('hide');
+            //Adiciona nome do Dish na array
+            arrOrderData[0] = placeNameInArray;
+            //Adiciona preço do Dish na array
+            arrOrderData[1] = placePriceInArray;
+        }
     }
-    if(elementPrincipal !== null){
-     elementPrincipal.classList.remove(menuSelected);
-     elementPrincipal.querySelector('ion-icon').classList.add('hide');
-     if(numeroSelecoes.includes(2)){
-        numeroSelecoes[1] = 0;
-     }
+    if (touch.getAttribute('data-test') === 'drink') {
+        if (arrOrderData[3] !== 0) {
+            arrOrderData[2] = '';
+            arrOrderData[3] = 0;
+        }
+        if (elementPastSelected !== null) {
+            elementPastSelected.classList.remove(menuSelected);
+            elementPastSelected.querySelector('ion-icon').classList.add('hide');
+        }
+        if (touchClassCheck) {
+            touch.classList.remove(menuSelected);
+            touch.querySelector('ion-icon').classList.add('hide');
+        } else {
+            touch.classList.add(menuSelected);
+            touch.querySelector('ion-icon').classList.remove('hide');
+            //Adiciona nome do Drink na array
+            arrOrderData[2] = placeNameInArray;
+            //Adiciona preço do Drink na array
+            arrOrderData[3] = placePriceInArray;
+        }
     }
-    if(elementPrincipal === selecionado || selecionado.classList.contains(menuSelected)){
-        selecionado.classList.remove(menuSelected);
-        selecionado.querySelector('ion-icon').classList.add('hide');
-    } else {
-     selecionado.classList.add(menuSelected);
-     selecionado.querySelector('ion-icon').classList.remove('hide');
-     if(!numeroSelecoes.includes(2)){
-         numeroSelecoes[1] = 2;
-     }
-     precoBebida = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')*100);
-     nomeBebida = selecionado.querySelector('h4').innerHTML;
+    if (touch.getAttribute('data-test') === 'dessert') {
+        if (arrOrderData[5] !== 0) {
+            arrOrderData[4] = '';
+            arrOrderData[5] = 0;
+        }
+        if (elementPastSelected !== null) {
+            elementPastSelected.classList.remove(menuSelected);
+            elementPastSelected.querySelector('ion-icon').classList.add('hide');
+        }
+        if (touchClassCheck) {
+            touch.classList.remove(menuSelected);
+            touch.querySelector('ion-icon').classList.add('hide');
+        } else {
+            touch.classList.add(menuSelected);
+            touch.querySelector('ion-icon').classList.remove('hide');
+            //Adiciona nome do Dessert na array
+            arrOrderData[4] = placeNameInArray;
+            //Adiciona preço do Dessert na array
+            arrOrderData[5] = placePriceInArray;
+        }
     }
-    checkingOut();
-}
-//Função para selecionar os items do menu de Sobremesas
-function menuSobremesas(selecionado){
-    const elementPrincipal = document.querySelector('.opcoesSobremesas>.menu-selected');
-    const menuSelected = 'menu-selected';
-    if(precoSobremesa !== 0 ){
-     precoSobremesa = 0;
-     }
-    if(elementPrincipal !== null){
-     elementPrincipal.classList.remove(menuSelected);
-     elementPrincipal.querySelector('ion-icon').classList.add('hide');
-     if(numeroSelecoes.includes(3)){
-        numeroSelecoes[2] = 0;
-     }
-    }
-    if(elementPrincipal === selecionado || selecionado.classList.contains(menuSelected)){
-        selecionado.classList.remove(menuSelected);
-        selecionado.querySelector('ion-icon').classList.add('hide');
-    } else {
-     selecionado.classList.add(menuSelected);
-     selecionado.querySelector('ion-icon').classList.remove('hide');
-     if(!numeroSelecoes.includes(3)){
-         numeroSelecoes[2] = 3;
-     }
-     precoSobremesa = Number(selecionado.querySelector('.price').innerHTML.replace('R$ ', '').replace(',', '.')*100);
-     nomeSobremesa = selecionado.querySelector('h4').innerHTML;
-    }
+    arrOrderData[6] = (arrOrderData[1] * 100 + arrOrderData[3] * 100 + arrOrderData[5] * 100);
     checkingOut();
 }
 //Função para alterar o botão de "Fechar pedido"
-function checkingOut(){
-    if(numeroSelecoes.includes(1) && numeroSelecoes.includes(2) && numeroSelecoes.includes(3)){
-        document.getElementById('checkout').classList.add('chk-available');
-        document.getElementById('checkout').innerHTML = "Fechar pedido";
-        document.getElementById('checkout').removeAttribute('disabled');
+function checkingOut() {
+    const checkoutDocument = document.getElementById('checkout');
+    if (arrOrderData[1] !== 0 && arrOrderData[3] !== 0 && arrOrderData[5] !== 0) {
+        checkoutDocument.classList.add('chk-available');
+        checkoutDocument.innerHTML = "Fechar pedido";
+        checkoutDocument.removeAttribute('disabled');
     } else {
-        document.getElementById('checkout').classList.remove('chk-available');
-        document.getElementById('checkout').innerHTML = "Selecione os 3 itens para fechar o pedido";
-        document.getElementById('checkout').setAttribute('disabled', '');
+        checkoutDocument.classList.remove('chk-available');
+        checkoutDocument.innerHTML = "Selecione os 3 itens para fechar o pedido";
+        checkoutDocument.setAttribute('disabled', '');
     }
 }
 //Função para abrir o overlay de revisar o pedido e aplicar mudanças de preços
-function chkoutBtn(){
-        if(clientName === "Não fornecido"){
-            clientName = prompt('Digite o seu nome');
-            if(clientName === null){
-                clientName = "Não fornecido";
-            }
+function chkoutBtn() {
+    if (clientName === noInput) {
+        clientName = prompt('Digite o seu nome');
+        if (clientName === null) {
+            clientName = noInput;
         }
-        if(clientAddress === "Não fornecido"){
-            clientAddress = prompt('Digite o seu endereço');
-            if(clientAddress === null){
-                clientAddress = "Não fornecido";
-            }
+    }
+    if (clientAddress === noInput) {
+        clientAddress = prompt('Digite o seu endereço');
+        if (clientAddress === null) {
+            clientAddress = noInput;
         }
-        subTotal = (precoPrincipal + precoBebida + precoSobremesa)/100;
-        document.getElementById('overlay').classList.remove('hide');
-        document.getElementById('nome-principal').innerHTML = nomePrincipal;
-        document.getElementById('preco-principal').innerHTML = (precoPrincipal/100).toFixed(2).toString().replace('.', ',');
-        document.getElementById('nome-bebida').innerHTML = nomeBebida;
-        document.getElementById('preco-bebida').innerHTML = (precoBebida/100).toFixed(2).toString().replace('.', ',');
-        document.getElementById('nome-sobremesa').innerHTML = nomeSobremesa;
-        document.getElementById('preco-sobremesa').innerHTML = (precoSobremesa/100).toFixed(2).toString().replace('.', ',');
-        document.getElementById('subtotal').innerHTML = "R$ " + subTotal.toFixed(2).toString().replace('.', ',');
+    }
+    document.getElementById('overlay').classList.remove('hide');
+    document.getElementById('nome-principal').innerHTML = arrOrderData[0];
+    document.getElementById('preco-principal').innerHTML = (arrOrderData[1]).toFixed(2).toString().replace('.', ',');
+    document.getElementById('nome-bebida').innerHTML = arrOrderData[2];
+    document.getElementById('preco-bebida').innerHTML = (arrOrderData[3]).toFixed(2).toString().replace('.', ',');
+    document.getElementById('nome-sobremesa').innerHTML = arrOrderData[4];
+    document.getElementById('preco-sobremesa').innerHTML = (arrOrderData[5]).toFixed(2).toString().replace('.', ',');
+    document.getElementById('subtotal').innerHTML = "R$ " + (arrOrderData[6] / 100).toFixed(2).toString().replace('.', ',');
 }
 //Função do botão cancelar do overlay de revisar o pedido
-function chkoutBtnCancel(){
+function chkoutBtnCancel() {
     document.getElementById('overlay').classList.add('hide');
 }
 //Função para encerrar o pedido e encaminhar para o Whatsapp
-function chkoutBtnEnd(){
-    subTotal = subTotal.toFixed(2);
-    const url ="https://wa.me/5555555555555?text=";
+function chkoutBtnEnd() {
+    const subTotal = (arrOrderData[6] / 100).toFixed(2);
+    const url = "https://wa.me/5555555555555?text=";
     const whatsappText = encodeURIComponent(`Olá, gostaria de fazer o pedido:
-- Prato: ${nomePrincipal}
-- Bebida: ${nomeBebida}
-- Sobremesa: ${nomeSobremesa}
+- Prato: ${arrOrderData[0]}
+- Bebida: ${arrOrderData[2]}
+- Sobremesa: ${arrOrderData[4]}
 Total: R$ ${subTotal}\n
 Nome: ${clientName}
 Endereço: ${clientAddress}`);
